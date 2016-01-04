ad811c77dfbeeb2b606193ea165edf616b36e47d
scut-wangjeff <630679072@qq.com>
2016-01-04 10:22:53 +0800

    添加申请

--- routes/qyh.js
+++ routes/qyh.js
@@ -12,4 +12,9 @@ router.get('/done', function(req, res, next) {
   res.render('done');
 });
 
+router.get('/apply', function(req, res, next) {
+  //res.send('respond with a resource');
+  res.render('apply');
+});
+
 module.exports = router;
